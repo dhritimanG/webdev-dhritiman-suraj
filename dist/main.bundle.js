e5c2501443624866ef3703d14d1991bb2a06b759
commit test on new mac <dhritiman.ganguly@outlook.com>
2017-12-11 12:58:46 -0500

    wax-creator

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -41,17 +41,26 @@ module.exports = module.exports.toString();
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_router__ = __webpack_require__("../../../router/@angular/router.es5.js");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
 
 var AppComponent = (function () {
-    function AppComponent() {
+    function AppComponent(router) {
+        this.router = router;
         this.title = 'title';
     }
+    AppComponent.prototype.ngOnInit = function () {
+        this.router.navigate(['/landing']);
+    };
     return AppComponent;
 }());
 AppComponent = __decorate([
@@ -60,9 +69,11 @@ AppComponent = __decorate([
         template: "<router-outlet></router-outlet>",
         // template: './app.component.html',
         styles: [__webpack_require__("../../../../../src/app/app.component.css")]
-    })
+    }),
+    __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1__angular_router__["b" /* Router */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1__angular_router__["b" /* Router */]) === "function" && _a || Object])
 ], AppComponent);
 
+var _a;
 //# sourceMappingURL=app.component.js.map
 
 /***/ }),
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'title';
+
+    constructor(private router: Router) { }
+    ngOnInit() {
+        this.router.navigate(['/landing']);
+    }
 }
+
